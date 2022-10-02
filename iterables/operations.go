c5d6db743330ef3185436149529ab3e73735c9b6
chris <christhirst@gmail.com>
2022-10-02 19:52:41 +0200

    added key extractor

---
+++ iterables/operations.go
@@ -0,0 +1,22 @@
+package iterables
+
+func findAndDelete(s []int, item int) []int {
+	index := 0
+	for _, i := range s {
+		if i != item {
+			s[index] = i
+			index++
+		}
+	}
+	return s[:index]
+}
+
+func GetMapKeys[K any](m map[string]K) []string {
+	i := 0
+	var keys []string
+	for k := range m {
+		keys[i] = k
+		i++
+	}
+	return keys
+}
