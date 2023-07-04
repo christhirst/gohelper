5e6ec3a8632ef5fdfcab7c489cce1a735bc6de54
chris <christhirst@gmail.com>
2023-07-04 08:55:37 +0200

    tests

--- ijson/convert_test.go
+++ ijson/convert_test.go
@@ -6,27 +6,28 @@ import (
 	"testing"
 )
 
-type Teststruct struct  {
+type Teststruct struct {
 	Field1 string `json:"field1"`
 	Field2 string `json:"field2"`
 	Field3 string `json:"field3"`
 }
 
-
 func TestRenderJSON(t *testing.T) {
-	got := Teststruct{Field1: "test1",Field2: "test2", Field3: "test3"}
+	//got := Teststruct{Field1: "test1", Field2: "test2", Field3: "test3"}
+	ss := []Teststruct{{Field1: "test1", Field2: "test2", Field3: "test3"}}
 	wr := httptest.NewRecorder()
-	RenderJSON(wr,got,200)
+	RenderJSON(wr, ss, 200)
+	fmt.Println("eee")
 	fmt.Println(wr.Body)
-	
+	fmt.Println("eee")
 	t.Error()
 }
 func TestJsonFromStruct(t *testing.T) {
-	got := Teststruct{Field1: "test1",Field2: "test2", Field3: "test3"}
+	got := Teststruct{Field1: "test1", Field2: "test2", Field3: "test3"}
 	//suits := []string{"Diamonds", "Hearts", "Clubs", "Spades"}
 	wr := httptest.NewRecorder()
-	JsonFromStruct(wr,got,200)
+	JsonFromStruct(wr, got, 200)
 	fmt.Println(wr.Body)
-	
+
 	t.Error()
 }
